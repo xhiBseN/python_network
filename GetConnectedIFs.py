15931bab843138c14cc5100831ea7cf9fe4fbd90
Philipp Speer <p.speer@gmx.net>
2018-06-17 13:58:05 +0200

    Changes to work with Python 3

--- GetConnectedIFs.py
+++ GetConnectedIFs.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-
 from getpass import getpass
 from netmiko import ConnectHandler
 
-username = raw_input('User Name: ')
+username = input('User Name: ')
 password = getpass()
 enapw = getpass('Enable Password:')
 
@@ -26,34 +24,34 @@ core2 = {
 devices =[core1, core2]
 
 for device in devices:
-	net_connect = ConnectHandler(**device)
-	net_connect.find_prompt()
-	output = net_connect.send_command('show int status | in connected')
-	iflist = list(enumerate(output.split('\n')))
-	ifs = []
-	for index,line in iflist:
-	        inf = []
-	        list = ' '.join(line.split()).split(' ')
-	        conf = net_connect.send_command('show run int '+list[0])
-	        inf.append(list[0])
-	        shint = net_connect.send_command('show int '+list[0]).split('\n')
-	        for speed in shint:
-	                if speed.find('duplex')>-1:
-	                        inf.append(speed)
-	        config = conf.split('\n',4)[4]
-	        inf.append(config)
-	        ifs.append(inf)
-	completeconfig = []
-        print '\n=============='
-        print device['ip']
-        print '==============\n'
-	print 'Interfaces:'
-	print '===========\n'
-	for interface in ifs:
-        	print interface[0]
-	        print interface[1]
-	        completeconfig.append(interface[2])
-	print '\nConfig:'
-	print '=========\n'
-	for cfg in completeconfig:
-	        print cfg
+    net_connect = ConnectHandler(**device)
+    net_connect.find_prompt()
+    output = net_connect.send_command('show int status | in connected')
+    iflist = list(enumerate(output.split('\n')))
+    ifs = []
+    for index,line in iflist:
+        inf = []
+        list = ' '.join(line.split()).split(' ')
+        conf = net_connect.send_command('show run int '+list[0])
+        inf.append(list[0])
+        shint = net_connect.send_command('show int '+list[0]).split('\n')
+        for speed in shint:
+            if speed.find('duplex')>-1:
+                inf.append(speed)
+        config = conf.split('\n',4)[4]
+        inf.append(config)
+        ifs.append(inf)
+    completeconfig = []
+    print('\n==============')
+    print(device['ip'])
+    print('==============\n')
+    print('Interfaces:')
+    print('===========\n')
+    for interface in ifs:
+        print(interface[0])
+        print(interface[1])
+        completeconfig.append(interface[2])
+    print('\nConfig:')
+    print('=========\n')
+    for cfg in completeconfig:
+        print(cfg)
